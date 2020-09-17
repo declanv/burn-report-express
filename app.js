5033f249d85aba382749800d12b5659f6f1fd3af
Declan Van Welie <declanv@gmail.com>
2020-09-17 14:33:29 -0400

    Adding: routing sendfile code back to app.js

--- app.js
+++ app.js
@@ -11,9 +11,9 @@ var app = express();
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-// app.get('/*', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
-// });
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 
 // view engine setup
